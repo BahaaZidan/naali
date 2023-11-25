9c5fe2ba9b29e1bc58a8aeae01f64105e1fd0368
BahaaZidan <gebnatorky@gmail.com>
2023-11-25 14:27:38 +0200

    install daisy ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,32 @@
 <script>
+	import { navigating, page, updated } from '$app/stores';
 	import '../app.css';
 </script>
+
+{#if $navigating}
+	<progress class="progress w-full"></progress>
+{/if}
+
+<nav>
+	<a href="/" aria-current={$page.url.pathname === '/'}>home</a>
+	<a href="/about" data-sveltekit-preload-data aria-current={$page.url.pathname === '/about'}
+		>about</a
+	>
+	<a href="/blog" aria-current={$page.url.pathname === '/blog'}>blog</a>
+</nav>
+
+{#if $updated}
+	<div class="toast toast-end">
+		<div class="alert alert-info">
+			<span>A new version of the app is available!</span>
+			<button
+				class="btn btn-primary"
+				on:click={() => {
+					location.reload();
+				}}>Reload</button
+			>
+		</div>
+	</div>
+{/if}
 
 <slot />
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-3xl text-cyan-300">about</h1>
+<p>this is the about page.</p>
