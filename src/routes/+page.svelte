8c7c9925bd05fc0c3b4731660d67bd559301a18d
BahaaZidan <gebnatorky@gmail.com>
2023-11-25 19:26:02 +0200

    basic home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1 class="text-3xl text-emerald-400">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero bg-base-200 py-5">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Write, earn, and support your favorite creators</h1>
+			<h2 class="text-5xl">All for $1</h2>
+			<p class="py-6">
+				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+				quasi. In deleniti eaque aut repudiandae et a id nisi.
+			</p>
+			<button class="btn btn-primary">Get Started</button>
+		</div>
+	</div>
+</div>
+
+<div
+	class="hero h-80"
+	style="background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);"
+>
+	<div class="hero-overlay bg-opacity-60"></div>
+	<div class="hero-content text-neutral-content text-center">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+			<p class="mb-5">
+				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+				quasi. In deleniti eaque aut repudiandae et a id nisi.
+			</p>
+			<button class="btn btn-primary">Get Started</button>
+		</div>
+	</div>
+</div>
