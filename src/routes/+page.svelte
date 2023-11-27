cad9fdf90e830a70bccdd9590b0c6d8143b55805
BahaaZidan <gebnatorky@gmail.com>
2023-11-27 19:28:27 +0200

    user avatar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 {#if $page.data.session?.user}
 	<p>Signed in as {$page.data.session.user.email}</p>
 	<button on:click={() => signOut()}>Sign out</button>
-	<img src="https://cdn.pixabay.com/photo/2017/08/11/19/36/vw-2632486_1280.png" />
+	<img class="h-10 w-10" src={$page.data.session.user.image} alt="user avatar" />
 {:else}
 	<div class="flex items-center gap-3 p-3">
 		<p class="text-2xl">Not signed in.</p>
