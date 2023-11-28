f6d2ae0d588badf9868c9df72a3780721ef6fdb5
BahaaZidan <gebnatorky@gmail.com>
2023-11-28 18:20:04 +0200

    login + logout flow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { navigating, page, updated } from '$app/stores';
+	import { signOut } from '@auth/sveltekit/client';
 	import '../app.css';
 </script>
 
@@ -7,44 +8,56 @@
 	<progress class="progress w-full"></progress>
 {/if}
 
-<nav class="navbar bg-base-100">
-	<div class="flex flex-1 gap-4">
+<nav class="navbar flex justify-between bg-base-100">
+	<div class="flex">
 		<a
 			class="link-hover font-mono text-2xl font-bold"
 			href="/"
 			aria-current={$page.url.pathname === '/'}>naali</a
 		>
-		<a class="link-hover font-mono" href="/about" aria-current={$page.url.pathname === '/about'}
-			>about</a
-		>
-		<a
-			class="link-hover font-mono"
-			href="/pricing"
-			aria-current={$page.url.pathname === '/pricing'}
-		>
-			pricing
-		</a>
+	</div>
+	<div class="flex flex-1 justify-center">
+		<input type="text" placeholder="Search" class="input input-bordered w-full max-w-md" />
+		<button></button>
 	</div>
 	<div class="flex flex-none gap-4 pr-3">
-		<a class="link-hover font-mono" href="/signup" aria-current={$page.url.pathname === '/signup'}
-			>create an account</a
-		>
-		<a class="link-hover font-mono" href="/login" aria-current={$page.url.pathname === '/login'}
-			>login</a
-		>
+		{#if $page.data.session?.user}
+			<div class="dropdown dropdown-end">
+				<div tabindex="0" role="button" class="avatar btn btn-circle btn-ghost">
+					<div class="w-10 rounded-full">
+						<img alt="User avatar" src={$page.data.session.user.image} />
+					</div>
+				</div>
+				<ul
+					class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
+				>
+					<li>
+						<a class="link-hover" href="/profile">Profile</a>
+					</li>
+					<li><a class="link-hover" href="/settings">Settings</a></li>
+					<li><button class="link-hover" on:click={() => signOut()}>Logout</button></li>
+				</ul>
+			</div>
+		{:else}
+			<a class="link-hover font-mono" href="/login" aria-current={$page.url.pathname === '/login'}>
+				login
+			</a>
+		{/if}
 	</div>
 </nav>
 
 {#if $updated}
 	<div class="toast toast-end">
-		<div class="alert alert-info">
+		<div class="alert alert-success">
 			<span>A new version of the app is available!</span>
 			<button
-				class="btn btn-primary"
+				class="btn btn-neutral btn-active"
 				on:click={() => {
 					location.reload();
-				}}>Reload</button
+				}}
 			>
+				Reload
+			</button>
 		</div>
 	</div>
 {/if}
