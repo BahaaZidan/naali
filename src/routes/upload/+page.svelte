9cf6cd36abcddf417e8095da0075ab89f5428121
BahaaZidan <gebnatorky@gmail.com>
2024-01-03 18:52:41 +0200

    use node + tus + uppy instead of s3

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,51 +1,39 @@
 <script lang="ts">
-	import { PUBLIC_R2_PUBLIC_ACCESS_URL } from '$env/static/public';
+	import { onMount } from 'svelte';
+	import Uppy from '@uppy/core';
+	import Tus from '@uppy/tus';
+	import DragAndDrop from '@uppy/drag-drop';
+	import ProgressBar from '@uppy/progress-bar';
+	import StatusBar from '@uppy/status-bar';
+	import { PUBLIC_TUS_URL } from '$env/static/public';
 
-	const handleFileUpload = async (e: Event) => {
-		const target = e.target as HTMLInputElement;
-		const file = target.files?.[0];
-
-		if (file) {
-			const getPresignedUrlResponse = await fetch('/upload', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: JSON.stringify({
-					fileName: file.name,
-					fileType: file.type
-				})
-			});
-
-			if (!getPresignedUrlResponse.ok) {
-				console.error('Failed to get presigned URL');
-			}
-
-			const { presignedUrl, objectKey } = await getPresignedUrlResponse.json();
-
-			const uploadToR2Response = await fetch(presignedUrl, {
-				method: 'PUT',
-				headers: {
-					'Content-Type': file.type
-				},
-				body: file
-			});
-
-			if (!uploadToR2Response.ok) {
-				console.error('Failed to upload file to R2');
-			} else {
-				console.log(`${PUBLIC_R2_PUBLIC_ACCESS_URL}/${objectKey}`);
-			}
-		}
-	};
+	onMount(() => {
+		const uppy = new Uppy({
+			allowMultipleUploads: false,
+			restrictions: { maxNumberOfFiles: 1, allowedFileTypes: ['.mp4'] }
+		});
+		uppy
+			.use(DragAndDrop, { target: '#drag-drop' })
+			.use(StatusBar, { target: '#status-bar' })
+			.use(Tus, { endpoint: PUBLIC_TUS_URL, limit: 6 });
+	});
 </script>
 
 <div class="flex h-screen flex-col items-center justify-center">
 	<h1 class="mb-3 text-3xl">Upload video</h1>
-	<input
+	<!-- <input
 		type="file"
 		accept="video/*"
 		class="file-input file-input-bordered w-full max-w-xs"
 		on:change={handleFileUpload}
-	/>
+	/> -->
+	<div id="drag-drop"></div>
+	<div id="status-bar"></div>
 </div>
+
+<style scoped>
+	@import '@uppy/core/dist/style.css';
+	@import '@uppy/drag-drop/dist/style.css';
+	@import '@uppy/progress-bar/dist/style.css';
+	@import '@uppy/status-bar/dist/style.css';
+</style>
