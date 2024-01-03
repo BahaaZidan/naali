0539ba522ad4b628d4e519f4e1063f6463c6432c
BahaaZidan <gebnatorky@gmail.com>
2024-01-03 18:55:02 +0200

    upgrade to sveltekit 2

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -3,6 +3,6 @@ import { redirect } from '@sveltejs/kit';
 export const load = async ({ locals }) => {
 	const session = await locals.getSession();
 	if (!session) {
-		throw redirect(307, '/login');
+		redirect(307, '/login');
 	}
 };
