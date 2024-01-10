ba84f9644b21ed0c925605140fc67de0b7384983
BahaaZidan <gebnatorky@gmail.com>
2024-01-10 11:35:50 +0200

    uppy + tus + cf stream

---
+++ src/routes/api/upload-url/+server.ts
@@ -0,0 +1,34 @@
+import { CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_STREAM_API_TOKEN } from '$env/static/private';
+import { error } from '@sveltejs/kit';
+
+export async function POST({ request }) {
+	const endpoint = `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/stream?direct_user=true`;
+
+	const length = request.headers.get('Upload-Length');
+	const metadata = request.headers.get('Upload-Metadata');
+
+	if (!length || !metadata) return error(400);
+
+	const response = await fetch(endpoint, {
+		method: 'POST',
+		headers: {
+			Authorization: `bearer ${CLOUDFLARE_STREAM_API_TOKEN}`,
+			'Tus-Resumable': '1.0.0',
+			'Upload-Length': length,
+			'Upload-Metadata': metadata
+		}
+	});
+
+	const destination = response.headers.get('Location');
+
+	if (!destination) return error(500);
+
+	return new Response(null, {
+		headers: {
+			'Access-Control-Expose-Headers': 'Location',
+			'Access-Control-Allow-Headers': '*',
+			'Access-Control-Allow-Origin': '*',
+			Location: destination
+		}
+	});
+}
--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -4,36 +4,45 @@
 	import Tus from '@uppy/tus';
 	import DragAndDrop from '@uppy/drag-drop';
 	import ProgressBar from '@uppy/progress-bar';
-	import StatusBar from '@uppy/status-bar';
-	import { PUBLIC_TUS_URL } from '$env/static/public';
+
+	import '@uppy/core/dist/style.css';
+	import '@uppy/drag-drop/dist/style.css';
+	import '@uppy/progress-bar/dist/style.css';
 
 	onMount(() => {
 		const uppy = new Uppy({
 			allowMultipleUploads: false,
+			debug: true,
+			autoProceed: true,
 			restrictions: { maxNumberOfFiles: 1, allowedFileTypes: ['.mp4'] }
 		});
-		uppy
-			.use(DragAndDrop, { target: '#drag-drop' })
-			.use(StatusBar, { target: '#status-bar' })
-			.use(Tus, { endpoint: PUBLIC_TUS_URL, limit: 6 });
+
+		const onUploadSuccess = el => (file, response) => {
+				const li = document.createElement('li');
+				const a = document.createElement('a');
+				a.href = response.uploadURL;
+				a.target = '_blank';
+				a.appendChild(document.createTextNode(file.name));
+				li.appendChild(a);
+
+				document.querySelector(el).appendChild(li);
+			};
+
+		
+			uppy
+				.use(DragAndDrop, { target: '#drag-drop-area' })
+				.use(Tus, { endpoint: '/api/upload-url', chunkSize: 150 * 1024 * 1024 })
+				.use(ProgressBar, { target: '.for-ProgressBar', hideAfterFinish: false })
+				.on('upload-success', onUploadSuccess('.uploaded-files ol'));
 	});
 </script>
 
 <div class="flex h-screen flex-col items-center justify-center">
 	<h1 class="mb-3 text-3xl">Upload video</h1>
-	<!-- <input
-		type="file"
-		accept="video/*"
-		class="file-input file-input-bordered w-full max-w-xs"
-		on:change={handleFileUpload}
-	/> -->
-	<div id="drag-drop"></div>
-	<div id="status-bar"></div>
+	<div id="drag-drop-area" style="height: 300px"></div>
+		<div class="for-ProgressBar"></div>
+		<button class="upload-button" style="font-size: 30px; margin: 20px">Upload</button>
+		<div class="uploaded-files" style="margin-top: 50px">
+			<ol></ol>
+		</div>
 </div>
-
-<style scoped>
-	@import '@uppy/core/dist/style.css';
-	@import '@uppy/drag-drop/dist/style.css';
-	@import '@uppy/progress-bar/dist/style.css';
-	@import '@uppy/status-bar/dist/style.css';
-</style>
