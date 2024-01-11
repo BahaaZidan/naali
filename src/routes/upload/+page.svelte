8fcbb33e2db1fc09a38c3ec36360e21ac76ef30c
BahaaZidan <gebnatorky@gmail.com>
2024-01-11 17:35:02 +0200

    allow multiple files + edit meta fields + redirect to profile on complete

--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -2,24 +2,27 @@
 	import { onMount } from 'svelte';
 	import Tus from '@uppy/tus';
 	import Uppy from '@uppy/core';
-import Dashboard from '@uppy/dashboard';
+	import Dashboard from '@uppy/dashboard';
+	import { page } from '$app/stores';
 
-import '@uppy/core/dist/style.min.css';
-import '@uppy/dashboard/dist/style.min.css';
-
+	import '@uppy/core/dist/style.min.css';
+	import '@uppy/dashboard/dist/style.min.css';
 
 	onMount(() => {
 		const uppy = new Uppy({
-			allowMultipleUploads: false,
-			debug: true,
-			autoProceed: true,
-			restrictions: { maxNumberOfFiles: 1, allowedFileTypes: ['.mp4'] }
+			restrictions: { allowedFileTypes: ['.mp4'] },
 		});
-
-
-			uppy
-				.use(Dashboard, { inline: true, target: '#uppy-dashboard',  })
-				.use(Tus, { endpoint: '/api/upload-url', chunkSize: 150 * 1024 * 1024 })
+		uppy
+			.use(Dashboard, { inline: true, target: '#uppy-dashboard', proudlyDisplayPoweredByUppy: false, 
+				showProgressDetails: true,
+				metaFields: [
+					{ id: 'name', name: 'Name', placeholder: 'file name' },
+				],
+		 	})
+			.use(Tus, { endpoint: '/api/upload-url', chunkSize: 150 * 1024 * 1024 })
+			.on("complete", () => {
+				window.location.href = `/user/${$page.data.session?.user?.id}`;
+			})
 	});
 </script>
 
