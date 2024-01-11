a84122d7b4d284c7529b321e251f6e82cdf264bf
BahaaZidan <gebnatorky@gmail.com>
2024-01-11 18:02:09 +0200

    fix typing

--- src/routes/user/[user_id]/+page.svelte
+++ src/routes/user/[user_id]/+page.svelte
@@ -1,29 +1,34 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	const videos = $page.data.videos;
+	export let data;
+	const videos = data.videos;
+	const user = data.user;
 </script>
 
 <div class="flex w-full flex-col items-center gap-3 bg-base-200 p-3">
 	<div class="avatar">
 		<div class="w-48 rounded-full">
-			<img src={$page.data.user?.image} alt="User avatar" />
+			<img src={user?.image} alt="User avatar" />
 		</div>
 	</div>
-	<h2 class="text-2xl font-semibold">{$page.data.user?.name}</h2>
-	<h4 class="text-lg">{$page.data.user?.email}</h4>
+	<h2 class="text-2xl font-semibold">{user?.name}</h2>
+	<h4 class="text-lg">{user?.email}</h4>
 	<button class="btn btn-active">follow</button>
 	<button class="btn btn-active">support</button>
 	<div class="divider my-0"></div>
-	<div class="flex gap-3 flex-wrap justify-center">
-	{#each videos as video}
-	<a href={`/video/${video.id}`}>
-		<div class="card w-96 bg-base-100 shadow-xl p-3">
-			<figure><img src={video.thumbnail} class="w-96 h-52 object-contain" alt="Thumbnail" /></figure>
-			<div class="card-body">
-				<h2 class="card-title line-clamp-2">{video.name}</h2>
-			</div>
+	{#if videos}
+		<div class="flex flex-wrap justify-center gap-3">
+			{#each videos as video}
+				<a href={`/video/${video.id}`}>
+					<div class="card w-96 bg-base-100 p-3 shadow-xl">
+						<figure>
+							<img src={video.thumbnail} class="h-52 w-96 object-contain" alt="Thumbnail" />
+						</figure>
+						<div class="card-body">
+							<h2 class="card-title line-clamp-2">{video.name}</h2>
+						</div>
+					</div>
+				</a>
+			{/each}
 		</div>
-	</a>
-	{/each}
-</div>
+	{/if}
 </div>
--- src/routes/video/[video_id]/+page.svelte
+++ src/routes/video/[video_id]/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import { PUBLIC_CLOUDFLARE_STREAM_CUSTOMER_CODE } from '$env/static/public';
-	const video = $page.data.video;
+	export let data;
+	const video = data.video;
 </script>
 
 <div style="position: relative; padding-top: 56.25%">
-  <iframe
+	<iframe
 		title="video"
-    src={`https://customer-${PUBLIC_CLOUDFLARE_STREAM_CUSTOMER_CODE}.cloudflarestream.com/${video.uid}/iframe`}
-    style="border: none; position: absolute; top: 0; height: 100%; width: 100%"
-    allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
-    allowfullscreen={true}
-  ></iframe>
+		src={`https://customer-${PUBLIC_CLOUDFLARE_STREAM_CUSTOMER_CODE}.cloudflarestream.com/${video.uid}/iframe`}
+		style="border: none; position: absolute; top: 0; height: 100%; width: 100%"
+		allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+		allowfullscreen={true}
+	></iframe>
 </div>
 <div class="flex flex-col">
 	<h2 class="text-3xl">{video.meta.name}</h2>
