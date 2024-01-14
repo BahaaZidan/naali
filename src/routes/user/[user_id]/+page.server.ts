d0cc711d83e47aebc37bb17d049ba9fb86d58451
BahaaZidan <gebnatorky@gmail.com>
2024-01-14 15:48:51 +0200

    list videos from db instead of hitting cf_stream rest endpoint

--- src/routes/user/[user_id]/+page.server.ts
+++ src/routes/user/[user_id]/+page.server.ts
@@ -1,38 +1,32 @@
-import { CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_STREAM_API_TOKEN } from '$env/static/private';
+import { PUBLIC_CLOUDFLARE_STREAM_CUSTOMER_CODE } from '$env/static/public';
 import { get_db } from '$lib/db';
-import { usersTable } from '$lib/db/schema.js';
-import type { Result, VideoDetails } from '$lib/types/cloudflare';
+import { usersTable, videos_table } from '$lib/db/schema.js';
 import { error } from '@sveltejs/kit';
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 
-export const load = async ({ params, locals, fetch }) => {
+export const load = async ({ params, locals }) => {
 	const db = get_db(locals.DB);
 	const user_id = params.user_id;
-	const user_result = await db.select().from(usersTable).where(eq(usersTable.id, user_id));
-	const user = user_result[0];
+	const user = (await db.select().from(usersTable).where(eq(usersTable.id, user_id)))[0];
 
 	if (!user) return error(404);
 
-	const cf_endpoint = `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/stream?creator=${user_id}`;
-
-	const videos_result = await (
-		await fetch(cf_endpoint, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `bearer ${CLOUDFLARE_STREAM_API_TOKEN}`
-			}
-		})
-	).json<Result<VideoDetails[]>>();
-
-	if (!videos_result.success) return error(500);
+	const logged_in_user_id = (await locals.getSession())?.user?.id;
+	const is_own_videos = logged_in_user_id === user_id;
+	const videos_result = await db
+		.select()
+		.from(videos_table)
+		.where(
+			is_own_videos
+				? eq(videos_table.creator, user_id)
+				: and(eq(videos_table.creator, user_id), eq(videos_table.publish_status, 'public'))
+		);
 
 	return {
 		user,
-		videos: videos_result.result?.map((v) => ({
-			name: v.meta.name,
-			id: v.uid,
-			thumbnail: v.thumbnail
+		videos: videos_result.map((v) => ({
+			...v,
+			thumbnail: `https://customer-${PUBLIC_CLOUDFLARE_STREAM_CUSTOMER_CODE}.cloudflarestream.com/${v.id}/thumbnails/thumbnail.jpg`
 		}))
 	};
 };
