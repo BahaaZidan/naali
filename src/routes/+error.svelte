07736bc36071b0a440b2fb1b69a45bc25f8df5ef
BahaaZidan <gebnatorky@gmail.com>
2024-01-14 20:48:21 +0200

    50vh in error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,4 +1,4 @@
-<div class="flex min-h-screen flex-col items-center justify-center">
+<div class="flex min-h-[50vh] flex-col items-center justify-center">
 	<h1 class="text-3xl font-bold">404</h1>
 	<h3 class="text-lg">Page not found.</h3>
 </div>
