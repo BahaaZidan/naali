0d7742752c4014c71ceaa6f76b4c238fd54135de
BahaaZidan <gebnatorky@gmail.com>
2024-01-15 17:05:20 +0200

    active indicator in menu

--- src/routes/studio/+layout.svelte
+++ src/routes/studio/+layout.svelte
@@ -1,14 +1,35 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import FullScreenContainer from '$lib/components/full-screen-container.svelte';
+
+	const routes = [
+		{
+			name: 'Settings',
+			route: '/studio'
+		},
+		{
+			name: 'Videos',
+			route: '/studio/videos'
+		},
+		{
+			name: 'Analytics',
+			route: '/studio/analytics'
+		},
+		{
+			name: 'Subtitles',
+			route: '/studio/subtitles'
+		}
+	];
 </script>
 
 <FullScreenContainer>
 	<div class="flex h-full">
-		<ul class="menu w-56 rounded-box bg-base-200">
-			<li><a href="/studio">Settings</a></li>
-			<li><a href="/studio/videos">Videos</a></li>
-			<li><a href="/studio/analytics">Analytics</a></li>
-			<li><a href="/studio/subtitles">Subtitles</a></li>
+		<ul class="menu menu-lg w-56 border-r border-gray-600">
+			{#each routes as route}
+				<li class="mb-2">
+					<a href={route.route} class:active={$page.url.pathname === route.route}>{route.name}</a>
+				</li>
+			{/each}
 		</ul>
 		<slot />
 	</div>
