79672527408311548f4ef1218928b700053372ad
BahaaZidan <gebnatorky@gmail.com>
2024-01-15 21:04:44 +0200

    menu touch up

--- src/routes/studio/+layout.svelte
+++ src/routes/studio/+layout.svelte
@@ -24,13 +24,15 @@
 
 <FullScreenContainer>
 	<div class="flex h-full">
-		<ul class="menu menu-lg w-56 border-r border-gray-600">
+		<ul class="menu menu-md w-56 border-r border-gray-600 p-0 px-2">
 			{#each routes as route}
 				<li class="mb-2">
 					<a href={route.route} class:active={$page.url.pathname === route.route}>{route.name}</a>
 				</li>
 			{/each}
 		</ul>
-		<slot />
+		<div class="px-3">
+			<slot />
+		</div>
 	</div>
 </FullScreenContainer>
