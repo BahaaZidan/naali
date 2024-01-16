14b53747df83ba878fa5e34e318db15330250de9
BahaaZidan <gebnatorky@gmail.com>
2024-01-16 13:23:14 +0200

    implement delete video

--- src/routes/studio/videos/+page.server.ts
+++ src/routes/studio/videos/+page.server.ts
@@ -1,8 +1,41 @@
+import { CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_STREAM_API_TOKEN } from '$env/static/private';
 import { PUBLIC_CLOUDFLARE_STREAM_CUSTOMER_CODE } from '$env/static/public';
 import { get_db } from '$lib/db';
 import { videos_table } from '$lib/db/schema';
-import { error } from '@sveltejs/kit';
-import { eq } from 'drizzle-orm';
+import { error, fail } from '@sveltejs/kit';
+import { and, eq } from 'drizzle-orm';
+
+export const actions = {
+	delete: async ({ locals, request }) => {
+		const formData = await request.formData();
+		const id = formData.get('id')?.toString();
+		if (!id) {
+			return fail(400, { id, missing: true });
+		}
+
+		const session = await locals.getSession();
+		const logged_in_user_id = session?.user?.id;
+		if (!logged_in_user_id) return fail(401);
+
+		const db = get_db(locals.DB);
+		const deletion = await db
+			.delete(videos_table)
+			.where(and(eq(videos_table.id, id), eq(videos_table.creator, logged_in_user_id)));
+		if (!deletion.success) return fail(500);
+
+		const endpoint = `https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_ACCOUNT_ID}/stream/${id}`;
+		const response = await fetch(endpoint, {
+			method: 'DELETE',
+			headers: {
+				Authorization: `bearer ${CLOUDFLARE_STREAM_API_TOKEN}`
+			}
+		});
+
+		if (response.status !== 200) return fail(500);
+
+		return { success: true };
+	}
+};
 
 export const load = async ({ locals }) => {
 	const session = await locals.getSession();
--- src/routes/studio/videos/+page.svelte
+++ src/routes/studio/videos/+page.svelte
@@ -27,12 +27,39 @@
 					<div class="dropdown">
 						<div tabindex="0" role="button" class="btn btn-ghost"><TablerDotsVertical /></div>
 						<ul class="menu dropdown-content z-[1] w-52 rounded-box bg-base-100 p-2 shadow">
-							<li><button>Delete</button></li>
+							<li>
+								<button
+									on:click={() => {
+										// @ts-ignore
+										document.getElementById(`dialog_delete_${video.id}`)?.showModal();
+									}}>Delete</button
+								>
+							</li>
 							<li><button>Edit</button></li>
 						</ul>
 					</div>
 				</td>
 			</tr>
+			<dialog id={`dialog_delete_${video.id}`} class="modal">
+				<div class="modal-box">
+					<h3 class="text-lg font-bold">Are you sure ?</h3>
+					<p>
+						You're about to delete <span class="font-bold italic">{video.name}</span>. This action
+						is irreversible! Are you sure you want to continue ?
+					</p>
+					<form method="POST" action="?/delete" id={`form_delete_${video.id}`}>
+						<input name="id" type="hidden" value={video.id} />
+					</form>
+					<div class="modal-action">
+						<form method="dialog">
+							<button class="btn">Close</button>
+						</form>
+						<button class="btn btn-error" type="submit" form={`form_delete_${video.id}`}
+							>Delete</button
+						>
+					</div>
+				</div>
+			</dialog>
 		{/each}
 	</tbody>
 </table>
