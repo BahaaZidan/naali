0e65044083dcde85f49a83c3987e2af73e6e4215
BahaaZidan <gebnatorky@gmail.com>
2024-01-16 12:52:56 +0200

    make actions the last column

--- src/routes/studio/videos/+page.svelte
+++ src/routes/studio/videos/+page.svelte
@@ -7,16 +7,22 @@
 <table class="table">
 	<thead>
 		<tr>
-			<th></th>
 			<th>Thumbnail</th>
 			<th>Name</th>
 			<th>Uploaded at</th>
 			<th>Status</th>
+			<th>Actions</th>
 		</tr>
 	</thead>
 	<tbody>
 		{#each data.videos as video}
 			<tr class="hover">
+				<td>
+					<img src={video.thumbnail} alt="thumbnail" class="h-16 w-28 object-contain" />
+				</td>
+				<td>{video.name}</td>
+				<td>{video.created_at}</td>
+				<td>{video.publish_status}</td>
 				<td class="p-0 px-3">
 					<div class="dropdown">
 						<div tabindex="0" role="button" class="btn btn-ghost"><TablerDotsVertical /></div>
@@ -26,12 +32,6 @@
 						</ul>
 					</div>
 				</td>
-				<td>
-					<img src={video.thumbnail} alt="thumbnail" class="h-16 w-28 object-contain" />
-				</td>
-				<td>{video.name}</td>
-				<td>{video.created_at}</td>
-				<td>{video.publish_status}</td>
 			</tr>
 		{/each}
 	</tbody>
