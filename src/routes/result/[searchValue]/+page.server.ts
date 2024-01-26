14ffb6d8333467cc70559a2d9aa6ba2f73d2baab
BahaaZidan <gebnatorky@gmail.com>
2024-01-26 17:53:13 +0200

    a slightly better search functionality (still not enough)

--- src/routes/result/[searchValue]/+page.server.ts
+++ src/routes/result/[searchValue]/+page.server.ts
@@ -1,10 +1,10 @@
 import { db } from '$lib/db';
 import { videosTable } from '$lib/db/schema';
-import { and, eq } from 'drizzle-orm';
+import { and, eq, ilike } from 'drizzle-orm';
 
 export const load = async ({ params }) => {
-	// TODO: fuzzy search
-	const videos = await db.select().from(videosTable).where(and(eq(videosTable.privacy, 'public'), eq(videosTable.name, params.searchValue)));
+	// TODO: use a search index
+	const videos = await db.select().from(videosTable).where(and(eq(videosTable.privacy, 'public'), ilike(videosTable.name, `%${params.searchValue}%`)));
 
 	return { videos };
 };
