e12f2f972e9cac0791f4dab9bc6ae60ea030e34c
BahaaZidan <gebnatorky@gmail.com>
2024-02-06 13:35:16 +0200

    fix auth by downgrading packages

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,13 +2,12 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/core/providers/github';
 import Google from '@auth/core/providers/google';
 import {
+	AUTH_SECRET,
 	GITHUB_ID,
 	GITHUB_SECRET,
-	AUTH_SECRET,
 	GOOGLE_CLIENT_ID,
 	GOOGLE_CLIENT_SECRET
 } from '$env/static/private';
-import type { Handle } from '@sveltejs/kit';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { db } from '$lib/db';
 import { accountsTable, sessionsTable, usersTable, verificationTokensTable } from '$lib/db/schema';
@@ -40,11 +39,7 @@ export const handle = SvelteKitAuth(async () => {
 			}
 		),
 		callbacks: {
-			session: async ({
-				session,
-				// @ts-expect-error wrong auth.js types
-				user
-			}) => {
+			session: async ({ session, user }) => {
 				if (session?.user) {
 					session.user.id = user.id;
 					// @ts-expect-error wrong auth.js types
@@ -55,4 +50,4 @@ export const handle = SvelteKitAuth(async () => {
 		}
 		// TODO: implement a logger ?
 	};
-}) satisfies Handle;
+});
