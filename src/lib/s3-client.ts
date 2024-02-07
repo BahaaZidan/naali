4efc1d1ef298a7e22b23a6ac920d247baa9c210c
BahaaZidan <gebnatorky@gmail.com>
2024-02-07 13:30:00 +0200

    start command + use full s3 endpoint

--- src/lib/s3-client.ts
+++ src/lib/s3-client.ts
@@ -1,9 +1,9 @@
 import { S3Client } from '@aws-sdk/client-s3';
-import { CLOUDFLARE_ACCOUNT_ID, R2_ACCESS_KEY_ID, R2_SECRET_ACCESS_KEY } from '$env/static/private';
+import { R2_ACCESS_KEY_ID, R2_S3_ENDPOINT, R2_SECRET_ACCESS_KEY } from '$env/static/private';
 
 export const s3 = new S3Client({
 	region: 'auto',
-	endpoint: `https://${CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+	endpoint: R2_S3_ENDPOINT,
 	credentials: {
 		accessKeyId: R2_ACCESS_KEY_ID,
 		secretAccessKey: R2_SECRET_ACCESS_KEY
